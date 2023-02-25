960d1f21f5b65139d8d7304ea78a888be7e38db2
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-02-25 14:48:07 -0300

    Ajustes no meio

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -6,21 +6,27 @@ const AboutUs: React.FC = () => {
         <section className={`flex flex-col bg-white py-20 text-xl md:text-2xl`}>
             <div className="container mx-auto px-11 text-black">
                 <p className="leading-tight max-w-5xl mx-auto text-2xl lg:text-3xl tracking-tight">
-                    <strong>Nosso objetivo com esse site é fazer com que você conheça mais sobre a insuficiência renal crônica (IRC) com o foco na alimentação.</strong>
-                    Com a intenção de tornar a explicação dinâmica, separa-se o seguinte site em, nossa equipe, os E-books produzidos por nossa equipe, algumas receitas e informações sobre a doença.
+                    <strong>
+                        Nosso objetivo com esse site é fazer com que você
+                        conheça mais sobre a insuficiência renal crônica (IRC)
+                        com o foco na alimentação.
+                    </strong>
+                    Com a intenção de tornar a explicação dinâmica, separa-se o
+                    seguinte site em, nossa equipe, os E-books produzidos por
+                    nossa equipe, algumas receitas e informações sobre a doença.
                 </p>
             </div>
             <div className="container max-auto px-11 text-center mt-28 text-black">
                 <h2>Nossa equipe</h2>
-                <div className="mt-10 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-4 gap-6 lg:gap-20">
-                    <Equipe id="1" name="Ana Alves"/>
-                    <Equipe id="2" name="Ana Siqueira"/>
-                    <Equipe id="3" name="Isabella"/>
-                    <Equipe id="4" name="Thaís"/>
+                <div className="mt-10 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-4 gap-6 lg:gap-20">
+                    <Equipe id="1" name="Ana Alves" />
+                    <Equipe id="2" name="Ana Siqueira" />
+                    <Equipe id="3" name="Isabella" />
+                    <Equipe id="4" name="Thaís" />
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default AboutUs
+export default AboutUs;
--- components/middle.tsx
+++ components/middle.tsx
@@ -1,66 +1,108 @@
 import React from "react";
-import { useCallback, useContext, useRef, useState } from 'react';
+import { useCallback, useContext, useRef, useState } from "react";
 import { ScrollContext } from "../utils/scroll-observer";
-import s from '../styles/middle.module.css'
-import Image from 'next/image'
+import s from "../styles/middle.module.css";
+import Image from "next/image";
 
 const opacityForBlock = (sectionProgress: number, blockNo: number) => {
-    const progress = sectionProgress - blockNo
-    if(progress >= 0 && progress < 1) return 1
-    return 0.2
-}
+    const progress = sectionProgress - blockNo;
+    if (progress >= 0 && progress < 1) return 1;
+    return 0.2;
+};
 
 const Middle: React.FC = () => {
-    const {scrollY} = useContext(ScrollContext)
-    const refContainer = useRef<HTMLDivElement>(null)
+    const { scrollY } = useContext(ScrollContext);
+    const refContainer = useRef<HTMLDivElement>(null);
 
-    const numOfPages = 3
-    let progress = 0
+    const numOfPages = 1.5;
+    let progress = 0;
 
-    const { current: elContainer } = refContainer
-    if(elContainer){
-        const { clientHeight, offsetTop } = elContainer
-        const screenH = window.innerHeight
-        const halfH = screenH/2
-        const percentY = Math.min(clientHeight + halfH, Math.max(-screenH, scrollY - offsetTop) + halfH) / clientHeight
-        progress = Math.min(numOfPages - 0.5, Math.max(0.5, percentY * numOfPages))
+    const { current: elContainer } = refContainer;
+    if (elContainer) {
+        const { clientHeight, offsetTop } = elContainer;
+        const screenH = window.innerHeight;
+        const halfH = screenH / 2;
+        const percentY =
+            Math.min(
+                clientHeight + halfH,
+                Math.max(-screenH, scrollY - offsetTop) + halfH
+            ) / clientHeight;
+        progress = Math.min(
+            numOfPages - 0.5,
+            Math.max(0.5, percentY * numOfPages)
+        );
     }
-
 
     return (
         <div ref={refContainer} className="text-white bg-black">
-            <div className="min-h-screen max-w-5xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-2xl md:text-3xl lg:text-4xl tracking-tight font-semibold">
+            <div className="min-h-screen max-w-7xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-xl md:text-2xl lg:text-3xl tracking-tight font-semibold">
                 <div className="leading-[1.15]">
-                    <div className={s.skillText}
-                    style={{
-                        opacity: opacityForBlock(progress, 0)
-                    }}>A insuficiência renal crônica é uma doença renal crônica progressiva e contínua, com os rins perdendo sua <strong>capacidade de filtragem</strong> do sangue e parando, aos poucos de funcionar, comprometendo também outras funções tradicionais dos rins.</div>
-                    <div className="items-center justify-center text-center self-center content-center p-10 w-auto">
-                        <div className={s.skillText}
+                    <div
+                        className={s.skillText}
                         style={{
-                            opacity: opacityForBlock(progress, 0)
-                        }}>
-                        <Image 
-                            src="/assets/middle.png"
-                            width={600}
-                            height={480}
-                            alt="cereais" />
+                            opacity: opacityForBlock(progress, 0),
+                        }}
+                    >
+                        Doença renal ou insuficiência renal é a condição na qual
+                        os rins perdem a capacidade de efetuar suas funções
+                        básicas.
+                    </div>
+                    <div
+                        className="{s.skillText}, text-center p-10 font-light text-xl md:text-2xl"
+                        style={{
+                            opacity: opacityForBlock(progress, 0),
+                        }}
+                    >
+                        A insuficiência renal pode ser:
+                    </div>
+                    <div
+                        className="{s.skillText}, font-light text-xl md:text-2xl grid grid-cols-1 md:grid-cols-[400px_minmax(200px,_1fr)_20px] lg:grid-cols-[600px_minmax(200px,_1fr)_100px]"
+                        style={{
+                            opacity: opacityForBlock(progress, 0),
+                        }}
+                    >
+                        <div className="list-item">
+                            <li>
+                                <p>
+                                    <strong className="font-bold">
+                                        Insuficiência renal aguda:{" "}
+                                    </strong>
+                                    ocorre quando há alguma lesão nos rins
+                                    provocando rápida queda nas suas funções;
+                                </p>
+                            </li>
+                            <li className="mt-10">
+                                <p>
+                                    <strong className="font-bold">
+                                        Insuficiência renal crônica:{" "}
+                                    </strong>
+                                    ocorre a perda parcial da função renal, de
+                                    forma lenta, progressiva e irreversível.
+                                    Quando é{" "}
+                                    <strong className="font-bold">
+                                        Terminal
+                                    </strong>
+                                    , leva ao aumento de toxinas e água no
+                                    organismo mais do que ele consegue suportar,
+                                    sendo necessário iniciar um tratamento que
+                                    substitua a função dos rins.
+                                </p>
+                            </li>
+                        </div>
+                        <div>
+                            <Image
+                                className="mt-10 ml-16 justify-self-center"
+                                src="/assets/middle.png"
+                                width={400}
+                                height={280}
+                                alt="cereais"
+                            />
                         </div>
                     </div>
-                    <span className={`${s.skillText} inline-block after:content-['_']`} style={{
-                        opacity: opacityForBlock(progress, 1)
-                    }}>
-                        Por isso a alimentação é essencial. Nas fases inicias da IRC, uma alimentação saudável <strong>inclui diversos cereais, sobretudo cereais integrais </strong>, frutas e legumes frescos.
-                    </span>
-                    <span className={`${s.skillText} inline-block`} style={{
-                        opacity: opacityForBlock(progress, 2)
-                    }}>
-                    Uma mudança fundamental para a sua alimentação pode ser a limitação da ingestão de proteínas, que vai lhe ajudar a preservar a função renal e a reduzir a quantidade de <strong>substâncias indesejáveis</strong> no sangue. Os alimentos ricos em proteínas são: carne, peixe, frango, ovos e produtos lácteos.
-                    </span>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default Middle
+export default Middle;

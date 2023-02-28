78c231888295c5d9b17ed6899074a6f8a9f1abea
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-02-28 12:43:01 -0300

    Ajustes

--- components/tratamentos.tsx
+++ components/tratamentos.tsx
@@ -1,7 +1,82 @@
 import React from "react";
 
 const Tratamento: React.FC = () => {
-    return <div></div>;
+    return (
+        <div className="text-white bg-black">
+            <div className="min-h-screen max-w-7xl mx-auto px-10 lg:px-20 py-24 md:py-28 lg:py-36 flex flex-col justify-center items-center text-xl md:text-2xl lg:text-3xl tracking-tight font-semibold">
+                <div>
+                    <div className="text-center p-10 text-xl md:text-3xl">
+                        Existem quatro tipos de tratamentos que podem ser:
+                    </div>
+                    <div className="font-light text-xl md:text-2xl grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2">
+                        <div>
+                            <div className="bg-lime-500 text-black p-10 list-item">
+                                <li>
+                                    <p>
+                                        <strong className="font-bold">
+                                            Conservador e pré-dialítico:{" "}
+                                        </strong>
+                                        que consiste em retardar a progressão da
+                                        doença, reduzindo a toxicidade urêmica,
+                                        a dieta hipoproteica e com baixo teor de
+                                        fósforo é recomendada para beneficar o
+                                        paciente, de modo a evitar sintomas
+                                        urêmicos;
+                                    </p>
+                                </li>
+                            </div>
+                            <div className="bg-lime-500 text-black pb-10 pt-3 px-10 mt-10 list-item">
+                                <li className="mt-10">
+                                    <p>
+                                        <strong className="font-bold">
+                                            Diálise Peritoneal:{" "}
+                                        </strong>
+                                        que consta em um procedimento no qual
+                                        utiliza a membrana semipermeável do
+                                        pertônio. Um catéter é implantado
+                                        cirugicamente no abdome até a cavidade
+                                        peritoneal. Um líquido dialisador rico
+                                        em dextrose é infundido, e, por difusão,
+                                        os produtos de excreção presentes no
+                                        sangue são levados por osmose através da
+                                        membrana peritoneal para o líquido
+                                        dialisador. Esse líquido é retirado e
+                                        descartado;
+                                    </p>
+                                </li>
+                            </div>
+                        </div>
+                        <div className="xl:ml-10">
+                            <div className="bg-lime-500 text-black pt-12 px-10 pb-24 list-item">
+                                <li>
+                                    <p>
+                                        <strong className="font-bold">
+                                            Hemodiálise:{" "}
+                                        </strong>
+                                        que resume-se em um tratamento mais
+                                        comum, no qual o sangue passa pela
+                                        membrana semipermeável do rim
+                                        artificial;
+                                    </p>
+                                </li>
+                            </div>
+                            <div className="bg-lime-500 text-black p-10 list-item mt-10">
+                                <li className="mt-10">
+                                    <p>
+                                        <strong className="font-bold">
+                                            Transplante renal:{" "}
+                                        </strong>
+                                        é uma opção de tratamento para os
+                                        pacientes que sofrem
+                                    </p>
+                                </li>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Tratamento;
--- components/treat.tsx
+++ components/treat.tsx
@@ -1,11 +1,37 @@
 import React from "react";
 import Image from "next/image";
+import { useCallback, useContext, useRef, useState } from "react";
+import { ScrollContext } from "../utils/scroll-observer";
 
 const Treat: React.FC = () => {
+    const [imageLoaded, setImageLoaded] = useState(false);
+    const refContainer = useRef<HTMLDivElement>(null);
+    const { scrollY } = useContext(ScrollContext);
+
+    let progress = 0;
+
+    const { current: elContainer } = refContainer;
+    if (elContainer) {
+        progress = Math.min(1, scrollY / elContainer.clientHeight);
+    }
+
+    const handleImageLoaded = useCallback(() => {
+        setImageLoaded(true);
+    }, []);
+
     return (
-        <section className={`bg-white h-half-screen xl:min-h-screen`}>
-            <div className="absolute overflow-hidden opacity-50">
-                <img src="/assets/treat.png" alt="background" />
+        <section
+            ref={refContainer}
+            className=" bg-white min-h-screen flex flex-col
+sticky top-0 -z-10"
+            style={{
+                transform: `transformY(-${progress * 20}vh)`,
+            }}
+        >
+            <div>
+                <div className="absolute  object-cover opacity-20">
+                    <img src="/assets/treat.png" alt="background" />
+                </div>
             </div>
             <div className="font-bold p-12 z-10 text-black drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)]">
                 <h2 className="text-2xl xl:text-5xl p-5 text-right">

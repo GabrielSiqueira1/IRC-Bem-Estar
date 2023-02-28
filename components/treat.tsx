cce910780baace7957e28c891775951582faaf79
GabrielSiqueira1 <gabrielsiqueira2765@gmail.com>
2023-02-28 13:36:03 -0300

    Refactoring

--- components/treat.tsx
+++ components/treat.tsx
@@ -22,7 +22,7 @@ const Treat: React.FC = () => {
     return (
         <section
             ref={refContainer}
-            className="bg-white min-h-screen flex flex-col
+            className="bg-white min-h-screen flex flex-col max-[600px]:items-center max-[600px]:justify-center
 sticky top-0 -z-10"
             style={{
                 transform: `transformY(-${progress * 20}vh)`,
@@ -34,7 +34,7 @@ sticky top-0 -z-10"
                 </div>
             </div>
             <div className="font-bold p-12 z-10 text-black drop-shadow-[0_5px_3px_rgba(0,0,0,0.4)]">
-                <h2 className="text-2xl xl:text-5xl p-5 text-right">
+                <h2 className="text-3xl xl:text-5xl p-5 text-right">
                     <span className="bg-lime-300 pb-5 p-7 border-2 border-lime-500 outline-2 outline-offset-8 border-spacing-96">
                         Tratamentos
                     </span>
